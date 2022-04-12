f550463df6d78a1fae4f731c445d44a0ac6075be
Faniry RABEMANANJARA <faniry.zo.rabemananjara@gmail.com>
2022-04-12 10:42:49 +0300

    Update server.js

--- AngularApp/server.js
+++ AngularApp/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/herokuubsmi'));
-
-app.get('/*', (req, res) = U + 003E res.sendFile('index.html', { root: 'dist/angular-heroku/' }), );
-
+app.use(express.static(__dirname + '/dist/angular-app'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/angular-app/index.html'));
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
